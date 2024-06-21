2ad3236132502ccb2c2391db864a7814e8b66c3c
Ze Shitcoin Webdev <112774130+zewebdev1337@users.noreply.github.com>
2024-06-21 12:38:50 -0600

    fix install script

--- install.sh
+++ install.sh
@@ -7,11 +7,11 @@ if [ ! -f .env ]; then
   read -p "Enter your Mistral API key: " mistral_api_key
   read -p "Enter your Codestral API key: " codestral_api_key
   read -p "Enter your OpenAI API key: " openai_api_key
-  echo "ANTHROPIC_API_KEY=${anthropic_api_key}" > .env
+  echo "ANTHROPIC_API_KEY=${anthropic_api_key}" >> .env
   echo "GOOGLE_API_KEY=${google_api_key}" >> .env
   echo "MISTRAL_API_KEY=${mistral_api_key}" >> .env
   echo "CODESTRAL_API_KEY=${codestral_api_key}" >> .env
-  echo "OPENAI_API_KEY=${openai_api_key}" > .env
+  echo "OPENAI_API_KEY=${openai_api_key}" >> .env
 fi
 
 python -m venv .venv
